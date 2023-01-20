44e1c421f2077c8004571746818a2f7d00236bff
Vlaminck Maxime <maxime.vlaminck@outlook.com>
2023-01-20 10:40:10 +0100

    test: add test for contains int

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -27,7 +27,7 @@ describe("validations tests suites - isValidGamerTag", () => {
     });
 
     test("should return true if the string is >= 8", () => {
-        const result = isValidGamerTag("Maxime20");
+        const result = isValidGamerTag("Maxime2@");
         expect(result).toEqual(true);
     });
 
@@ -44,5 +44,15 @@ describe("validations tests suites - isValidGamerTag", () => {
     test("should return false if the string doesn't contains a special caracter", () => {
         const result = isValidGamerTag("Maxime2003");
         expect(result).toEqual(false);
+    });
+
+    test("should return false if the string doesn't contains int", () => {
+        const result = isValidGamerTag("Maximeaaaaaaa@");
+        expect(result).toEqual(false);
+    });
+
+    test("should return true if the string contains a int", () => {
+        const result = isValidGamerTag("Maxime2003@");
+        expect(result).toEqual(true);
     });
 });
--- src/services/validations.js
+++ src/services/validations.js
@@ -4,7 +4,10 @@ const isEmpty = (label) => !label || label.length === 0;
 
 function isValidGamerTag(nameTag){
     if(nameTag.length<8) return false;
-    
+
+    const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+    if(specialChars.test(nameTag) == false) return false;
+
     return true;
 }
 
