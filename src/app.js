4e2732212051d03295d94da7cef5d9164033f159
Vlaminck Maxime <maxime.vlaminck@outlook.com>
2023-01-20 10:45:45 +0100

    feat: add logic display for the feedback message

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 /* istanbul ignore file */
 try {
-    const { isEmpty } = require("./services/validations");
+    const { isEmpty, isValidGamerTag } = require("./services/validations");
     const gamerTagInput = document.getElementById("gamerTagInput");
     const checkButton = document.getElementById("gamerTagCheckButton");
     const feedbackMessage = document.getElementById("gamerTagFeedback");
@@ -17,6 +17,9 @@ try {
         feedbackMessageText = isEmpty(gamerTagValue)
             ? "Gamer tag cannot be empty"
             : "Gamer tag is valid";
+        feedbackMessage.textContent = feedbackMessageText;
+
+        feedbackMessageText = isValidGamerTag(gamerTagValue) ? "Your gamerTag is valid :)" : "Your gamerTag is not valid";
         feedbackMessage.textContent = feedbackMessageText;
     });
 } catch (err) {
